84a5df189a8583f3dab334f5beb3d848a4a9ce8a
TajSapraHousing <taj.sapra@housing.com>
2023-07-15 00:34:28 +0530

    Task 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState('World')
+  const handleLogin=()=>{
+    setData('Taj')
+  }
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <p>Hello {data}</p>
+      <button onClick={handleLogin}>Click Me</button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.hydrateRoot(document.getElementById('root'), <App />);
